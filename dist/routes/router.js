075a343a793a83786fdc1316f0c7ea6122ff6b02
Caio Carvalho <caioalmeidaacl@gmail.com>
2025-05-20 12:08:42 -0300

    compiled version

--- dist/routes/router.js
+++ dist/routes/router.js
@@ -1,14 +1,10 @@
 import { Router } from "express";
-import { actionsController } from "../controllers/ActionController";
-
+import { actionsController } from "../controllers/ActionController.js";
 const router = Router();
-
 router.post("/api/download", (req, res) => {
   actionsController.download(req, res);
 });
-
 router.post("/api/title", (req, res) => {
   actionsController.getTitle(req, res);
 });
-
 export { router };
---
+++ src/controllers/ActionController.ts
@@ -0,0 +1,65 @@
+import { Request, Response } from "express";
+import { spawn, spawnSync } from "child_process";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+class ActionsController {
+  async download(req: Request, res: Response) {
+    const { url, quality } = req.body;
+
+    if (!url || !url.startsWith("http")) {
+      return res.status(400).json({ error: "Invalid URL" });
+    }
+
+    const selectedFormat = quality || "best";
+
+    res.setHeader("Content-Type", "video/mp4");
+
+    const process = spawn("yt-dlp", ["-f", selectedFormat, "-o", "-", url]);
+
+    process.stdout.pipe(res);
+
+    process.stderr.on("data", (data) => {
+      console.error(`stderr: ${data}`);
+    });
+
+    process.on("close", (code) => {
+      if (code !== 0) {
+        console.error(`yt-dlp exited with code ${code}`);
+        if (!res.headersSent) res.status(500).end("Download failed");
+      }
+    });
+  }
+
+  getTitle(req: Request, res: Response) {
+    const { url } = req.body;
+
+    if (!url || !url.startsWith("http")) {
+      return res.status(400).json({ error: "URL inválida" });
+    }
+
+    try {
+      const titleResult = spawnSync("yt-dlp", ["--get-title", url]);
+
+      if (titleResult.status !== 0) {
+        const stderr = titleResult.stderr.toString();
+        console.error("Erro ao buscar título:", stderr);
+        return res.status(500).json({ error: "Erro ao obter o título" });
+      }
+
+      const title = titleResult.stdout.toString().trim();
+
+      if (!title) {
+        return res.status(500).json({ error: "Título não encontrado" });
+      }
+
+      res.json({ title });
+    } catch (err) {
+      console.error("Erro inesperado:", err);
+      res.status(500).json({ error: "Erro interno do servidor" });
+    }
+  }
+}
+
+export const actionsController = new ActionsController();
