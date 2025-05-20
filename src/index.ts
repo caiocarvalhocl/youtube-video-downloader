3cd9cf5089178b80e64272fd6bee936033e5b8bc
Caio Carvalho <caioalmeidaacl@gmail.com>
2025-05-20 12:35:42 -0300

    fix build

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,6 @@
 import express from "express";
 import cors from "cors";
 import { router } from "./routes/router";
-import path from "path";
-import { fileURLToPath } from "url";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 const app = express();
 const PORT = process.env.PORT || 4000;
@@ -13,8 +8,6 @@ const PORT = process.env.PORT || 4000;
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-app.use("/downloads", express.static(path.join(__dirname, "..", "downloads")));
 
 app.use(router);
 
